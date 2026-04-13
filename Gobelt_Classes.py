52f4c2ca6f305dba3449b351002e01d6f9238fe8
Vanessa <luckyllama2009@gmail.com>
2026-04-13 15:26:11 -0400

    Added comments

--- Gobelt_Classes.py
+++ Gobelt_Classes.py
@@ -1,36 +1,48 @@
 
 from enum import Enum
 
+#Class that contains a collection of player colors as numbers 
 class player_color_class(Enum):
     
-    dark = (0, 0, 0)
-    light = (255, 255, 255)
+    dark = 2
+    light = 1
     initial = 0
 
+#Class contains all the attributes and methods of a player piece
 class player_piece_class:
     
     color = player_color_class.initial
     size = 0
     
+    #Sets the color and size of a player piece
     def __init__(self, in_color, in_size):
         self.color = in_color
         self.size = in_size
 
+    #Defines what to do when print() is called for a player piece
     def __str__(self):
         piece_string = 'XXX'
 
+        #If piece size is 0, print dash
+        if self.size == 0:
+            piece_string = " - "
+        
+        #If piece has a valid color, print piece size with assigned background color
         if self.color == player_color_class.dark:
             piece_string = f"\033[42m {self.size} \033[0m"
         elif self.color == player_color_class.light:
             piece_string = f"\033[45m {self.size} \033[0m"
 
-        if self.size == 0:
-            piece_string = " - "
-        
         return piece_string
+    
+    def __bool__(self):
+        return self.size != 0
 
+#Class that contains all the attributes and methods of the stacks of the unplayed pieces in the player boards
 class player_board_class:
     
+    #Creates a list of 3 lists where each list contains 4 different sized pieces,
+    # stacked in order of smallest to largest in the chosen color
     def __init__(self, new_player_color: player_color_class):
         self.color = new_player_color
         NUM_PLAYER_STACKS = 3
@@ -40,40 +52,50 @@ class player_board_class:
                 new_piece = player_piece_class(new_player_color, piece_size)
                 self.player_stacks[player_stack].append(new_piece)
 
+    #Removes the piece from the top of the stack at the passed in coordinate for player pieces and returns it
     def get_piece(self, coord):
 
         if not self.player_stacks[coord]:
             return None
         return self.player_stacks[coord].pop()
     
-    def top_piece(self, coord):
+    #Returns the piece at the top of the stack without removing it, at the passed in coordinate for player pieces
+    #Allows viewing of the top piece without removing it
+    def check_top_piece(self, coord):
         if len(self.player_stacks[coord]) == 0:
             return player_piece_class(player_color_class.initial, 0)
         return self.player_stacks[coord][-1]
 
+#Class that contains all the attributes and methods of the gameboard and the played pieces 
 class game_board_class:
     
     ROWS = 4
     COLS = 4
     
+    #Creates 2d list of stacks containing null pieces to represent empty 
     def __init__(self):
         self.board_stacks = [[[] for _ in range(self.COLS)] for _ in range(self.ROWS)]
 
         for row in range(4):
             for col in range(4):
-                new_piece = player_piece_class(player_color_class.initial, 0)
+                new_piece = player_piece_class(player_color_class, 0)
                 self.board_stacks[row][col].append(new_piece)
     
-    def top_piece(self, row, col):
-        return self.board_stacks[row][col][-1]
-
+    #Removes the piece from the top of the stack at the passed in coordinate for player pieces and returns it
+    # Only returns if it is a valid piece otherwise returns none
     def get_piece(self, row_up, col_up):
 
-        top_piece = self.board_stacks[row_up][col_up][-1]
-        if top_piece.size == 0 and top_piece.color == player_color_class.initial:
+        check_top_piece = self.board_stacks[row_up][col_up][-1]
+        if check_top_piece.size == 0:
             return None
         return self.board_stacks[row_up][col_up].pop()
 
+    #Returns the piece at the top of the stack without removing it, 
+    # Allows for viewing of the top piece without removing it
+    def check_top_piece(self, row, col): 
+        return self.board_stacks[row][col][-1]
+
+    #Puts piece on game board by appending to a stack at the coordinates on the game board
     def put_piece(self, row_down, col_down, game_piece):
 
         self.board_stacks[row_down][col_down].append(game_piece)
--- Goblet_Functions.py
+++ Goblet_Functions.py
@@ -5,29 +5,55 @@ import pickle
 
 full_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'autosave.pkl')
 
+#Prints text explaining the function of the program
 def print_program_instructions():
     print('Welcome to Goblet!')
     print('Valid pick up locations: ')
     print('Player board: a b c')
-    print('Gameboard: coordinates 00 to 33')
+    print('Gameboard: coordinates 00 to 33 as colum then row')
     print('Game autosaves after each turn.')
 
+#Collects input for what the starting player color is and applies it
+def select_start_player(player_light, player_dark):
+    
+    while True:
+            player_start = (input('Who starts the game? (l/d) ')).lower()
+
+            #If light was selected, current player is light
+            if player_start == 'l':
+                current_player = player_light
+                return current_player
+
+            #If dark was selected, current player is dark
+            elif player_start == 'd':
+                current_player = player_dark
+                return current_player
+
+            print('Unvalid player selection, please try again.')
+
+#Prints the current state of the gameboard with reference coordinates
 def print_game_board(game_board):
     print('Gameboard: ')
+    print('   0  1  2  3 ')
     for col in range(4):
+        print(f'{col} ', end='')
         for row in range(4):
-            print(game_board.top_piece(row, col), end="")
+            print(game_board.check_top_piece(row, col), end="")
         print(" ")
 
+#Prints the current state of the player board of passed in player
 def print_player_board(player_board):
     print('Player Board: ', end="")
     for stack_num in range(3):
-        print(player_board.top_piece(stack_num), end="")
+        print(player_board.check_top_piece(stack_num), end="")
         print(" ", end="")
     print(" ")
 
+#Saves the current game state by overwriting the previous save
 def save_game(full_path, game_board, player_light, player_dark, current_player):
 
+    #Opens the save file, assigns current player id to a string based on who the current player is, 
+    # saves the game data as a dictionary using pickle, closes file automatically using 'with'
     with open(full_path, 'wb') as file_save:
         if current_player == player_light:
             current_player_id = 'player_light'
@@ -41,8 +67,11 @@ def save_game(full_path, game_board, player_light, player_dark, current_player):
             'current_player_id': current_player_id
         }, file_save)
 
+#Loads file containing game state from previous game
 def load_game(full_path):
 
+    #Opens the file, extracts data using pickle, restores current player to a player based on who the current player was during the save,
+    # returns all loaded game objects, closes file using 'with'
     with open(full_path, 'rb') as file_load:
         game_data = pickle.load(file_load)
 
@@ -53,6 +82,7 @@ def load_game(full_path):
     
     return game_data['game_board'], game_data['player_light'], game_data['player_dark'], current_player
 
+#Loops collecting input from player for location of picking up a piece until input is confirmed to be a valid pickup location
 def user_interface_pick(current_player):
 
     which_board = 0
@@ -62,12 +92,14 @@ def user_interface_pick(current_player):
     while True:
         pick_up = input(f"Pick up piece {current_player_print}: ").strip().lower()
 
+        #If input is coordinates, makes sure that they are within valid range before returning
         if len(pick_up) == 2 and pick_up.isdigit():
             which_board = 0
             row_up, col_up = map(int, pick_up)
             if 0 <= row_up <=3 and 0 <= col_up <= 3:
                 return which_board, row_up, col_up
 
+        #If input is a letter, makes that that it is within valid ascii range before returning
         elif len(pick_up) == 1:
             
             letter_val = ord(pick_up)
@@ -81,6 +113,7 @@ def user_interface_pick(current_player):
 
         print('Unvalid pick up location, please try again.')
 
+#Loops collecting input from player for location of putting down a piece until input is confirmed to be a valid put down location
 def user_interface_put(current_player):
     
     current_player_print = current_player.color.name.capitalize()
@@ -88,6 +121,7 @@ def user_interface_put(current_player):
     while True:
         put_down = input(f"Put down piece {current_player_print}: ").strip()
 
+        #Confirms that input is coordinates and it is within the valid range before returning
         if len(put_down) == 2 and put_down.isdigit():
             row_down, col_down = map(int, put_down)
 
@@ -96,6 +130,7 @@ def user_interface_put(current_player):
 
         print('Unvalid put down location, please try again')
 
+#Once pick up location is confirmed to be valid, loops until the check that the piece at that location is valid
 def player_pick(game_board, current_player):
 
     game_piece = None
@@ -106,22 +141,23 @@ def player_pick(game_board, current_player):
 
         if which_board == 0:
 
-            board_game_piece = game_board.get_piece(row_up, col_up)
+            top_piece = game_board.check_top_piece(row_up, col_up)
 
-            if board_game_piece is None:
+            #If there is no valid piece on the game board, try again
+            if top_piece is None:
                 print('You cannot pick up there, try again.')
                 continue
 
-            top_piece = game_board.top_piece(row_up, col_up)
-
+            #If the piece color does not match current player color, try again
             if top_piece.color != current_player.color:
                 print('You can only pick up your own piece, please try again.')
                 continue
 
-            game_piece = board_game_piece
+            game_piece = game_board.get_piece(row_up, col_up)
 
         elif which_board == 1:
 
+            #If there is no piece on the player board, try again
             player_game_piece = current_player.get_piece(row_up)
             if player_game_piece is None:
                 print('You cannot pick up there, try again.')
@@ -134,64 +170,53 @@ def player_pick(game_board, current_player):
 
         return game_piece
 
+#Once put down location is confirmed to be valid, loops until the check that the piece at that location is valid
 def player_put(game_piece, game_board, current_player):
 
     while True:
         row_down, col_down = user_interface_put(current_player)
 
-        board_piece = game_board.top_piece(row_down, col_down).size
+        board_piece = game_board.check_top_piece(row_down, col_down).size
 
+        #Only allows the piece to be put down if the piece at that location already is smaller than the picked up piece
         if game_piece.size > board_piece:
             game_board.put_piece(row_down, col_down, game_piece)
             break
         else:
             print('You cannot place there, try again.')
- 
-def select_start_player(player_light, player_dark):
-    
-    while True:
-            player_start = (input('Who starts the game? (l/d) ')).lower()
 
-            if player_start == 'l':
-                current_player = player_light
-                return current_player
-
-            elif player_start == 'd':
-                current_player = player_dark
-                return current_player
-
-            print('Unvalid player selection, please try again.')
-
+#Checks if there are 4 pieces of the same color in the rows, colums, and diagonals, if yes then returns True and who won
 def check_win(game_board):
             
     #Checks wins in rows
     for row in range(4):
-        first_piece = game_board.top_piece(row, 0)
-        if first_piece.color != player_color_class.initial:
-            if all(game_board.top_piece(row, col).color == first_piece.color for col in range(4)):
+        first_piece = game_board.check_top_piece(row, 0)
+        if first_piece:
+            if all(game_board.check_top_piece(row, col).color == first_piece.color for col in range(4)):
                 return True, first_piece.color.name
     
     #Check wins in colums
     for col in range(4):
-        first_piece = game_board.top_piece(0, col)
-        if first_piece.color != player_color_class.initial:
-            if all(game_board.top_piece(row, col).color == first_piece.color for row in range(4)):
+        first_piece = game_board.check_top_piece(0, col)
+        if first_piece:
+            if all(game_board.check_top_piece(row, col).color == first_piece.color for row in range(4)):
                 return True, first_piece.color.name
     
     #Checks win on top-left to bottom-right diagonal
-    first_piece = game_board.top_piece(0, 0)
-    if first_piece.color != player_color_class.initial:
-        if all(game_board.top_piece(i, i).color == first_piece.color for i in range(4)):
+    first_piece = game_board.check_top_piece(0, 0)
+    if first_piece:
+        if all(game_board.check_top_piece(i, i).color == first_piece.color for i in range(4)):
             return True, first_piece.color.name
     
     #Checks win on top-right to bottom-left diagonal
-    first_piece = game_board.top_piece(0, 3)
-    if first_piece.color != player_color_class.initial:
-        if all(game_board.top_piece(i, 3 - i).color == first_piece.color for i in range(4)):
+    first_piece = game_board.check_top_piece(0, 3)
+    if first_piece:
+        if all(game_board.check_top_piece(i, 3 - i).color == first_piece.color for i in range(4)):
             return True, first_piece.color.name
     
     return False, None
 
+#Checks if a player won, if yes prints game_board (to see 4 in a row), game over, who the winner is, and returns True
 def detect_win(game_board):
 
     if_win, winner_color = check_win(game_board)
